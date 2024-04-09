b93fe219f86a337c94b41ac0a48de157481b5a05
Mario Pranata <mario.pranata@gopay.co.id>
2024-04-09 23:24:39 +0700

    fix: lint error

--- src/components/MainPage/HomePage/HomePage.jsx
+++ src/components/MainPage/HomePage/HomePage.jsx
@@ -12,7 +12,7 @@ export default function HomePage() {
         <div>
           <h2>My name is</h2>
           <h3 className='teal-text'>Mario Edgar Pranata</h3>
-          <h4 className=''>I'm a coder and I like to learn new things</h4>
+          <h4>{"I'm a coder and I like to learn new things"}</h4>
         </div>
       </div>
     </div>
